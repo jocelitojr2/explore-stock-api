a21ee74ecdf33213e26670d5a01ac21cb23ed3cd
Jocelito Oliveira Costa Junior <jocelito64@hotmail.com>
2024-03-05 17:46:07 -0300

    first commit

---
+++ src/database/knex/migrations/20230811164833_createUsers.js
@@ -0,0 +1,15 @@
+exports.up = knex => knex.schema.createTable("users", table => {
+  table.increments("id");
+  table.text("name").notNullable();
+  table.text("email").notNullable();
+  table.text("password").notNullable();
+
+  table
+    .enum("role", ["admin", "customer", "sale"], { usedNative: true, enumName: "roles" })
+    .notNullable().defaultTo("customer");
+
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable("users");
